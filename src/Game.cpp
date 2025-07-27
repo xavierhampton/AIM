f7298cc2cd91cb394ffd5852a5fac096e83f598d
Xavier Hampton <xaviermichaelhampton@gmail.com>
2025-07-27 05:36:17 -0500

    moved shaders to shader directory

--- src/Game.cpp
+++ src/Game.cpp
@@ -3,28 +3,6 @@
 #include <iostream>
 #include <math.h>
 
-//TEMP STORAGE
-
-//This is the source code for the vertex shader
-const char*vertexShaderSource = "#version 330 core\n"
-"layout(location = 0) in vec3 aPos;\n"
-"layout(location = 1) in vec3 aColor;\n"
-"out vec3 ourColor;\n"
-"void main()\n"
-"{\n"
-"    gl_Position = vec4(aPos.x,aPos.y, aPos.z, 1.0);\n"
-"    ourColor = aColor;\n"
-"}\0";
-
-//This is the source code for the fragment shader
-const char* fragmentShaderSource = "#version 330 core\n"
-"out vec4 FragColor;\n"
-"in vec3 ourColor;\n"
-"void main()\n"
-"{\n"
-"    FragColor = vec4(ourColor, 1.0);\n"
-"}\0";
-
 Game::Game(int width, int height, const char* title) {
     window = new Window(width, height, title);
     if (!window->isInitialized()) {
