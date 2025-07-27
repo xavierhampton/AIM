20a74217937300c95e3460e5f1971033ba5b81fd
Xavier Hampton <xaviermichaelhampton@gmail.com>
2025-07-27 05:33:37 -0500

    shader utilities

--- src/Game.cpp
+++ src/Game.cpp
@@ -8,18 +8,21 @@
 //This is the source code for the vertex shader
 const char*vertexShaderSource = "#version 330 core\n"
 "layout(location = 0) in vec3 aPos;\n"
+"layout(location = 1) in vec3 aColor;\n"
+"out vec3 ourColor;\n"
 "void main()\n"
 "{\n"
 "    gl_Position = vec4(aPos.x,aPos.y, aPos.z, 1.0);\n"
+"    ourColor = aColor;\n"
 "}\0";
 
 //This is the source code for the fragment shader
 const char* fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
-"uniform vec4 ourColor;\n"
+"in vec3 ourColor;\n"
 "void main()\n"
 "{\n"
-"    FragColor = ourColor;\n"
+"    FragColor = vec4(ourColor, 1.0);\n"
 "}\0";
 
 Game::Game(int width, int height, const char* title) {
@@ -30,10 +33,11 @@ Game::Game(int width, int height, const char* title) {
     }
 
        float vertices[] = {
-    -0.5f, -0.5f, 0.0f,
-     0.5f, -0.5f, 0.0f,
-     0.0f,  0.5f, 0.0f
-    }; 
+    // positions         // colors
+     0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
+    -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
+     0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top 
+};    
 
     //Generate a Vertex Array Object (VAO) and bind it
     glGenVertexArrays(1, &VAO);
@@ -65,8 +69,12 @@ Game::Game(int width, int height, const char* title) {
     glDeleteShader(fragmentShader);
 
     //Set up vertex attributes
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    // Position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+    // Color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
 }
 
 Game::~Game() {
--- src/Shader.cpp
+++ src/Shader.cpp
@@ -79,3 +79,19 @@ Shader::Shader(const char* vertexPath, const char* fragmentPath)
     glDeleteShader(fragment);
 
 };
+
+void Shader::use() {
+    glUseProgram(ID);
+}
+
+void Shader::setBool(const std::string& name, bool value) const {
+    glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
+}
+
+void Shader::setInt(const std::string& name, int value) const {
+    glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
+}
+
+void Shader::setFloat(const std::string& name, float value) const {
+    glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
+}
