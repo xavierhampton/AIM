b435d41a0f38e2ffc5969ecdc9e327422900fdea
Xavier Hampton <xaviermichaelhampton@gmail.com>
2025-08-10 04:47:46 -0500

    added license to LICENSE, decluttered code base

--- src/raylib_game.c
+++ src/raylib_game.c
@@ -1,16 +1,3 @@
-/*******************************************************************************************
-*
-*   raylib game template
-*
-*   <Game title>
-*   <Game description>
-*
-*   This game has been created using raylib (www.raylib.com)
-*   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
-*
-*   Copyright (c) 2021 Ramon Santamaria (@raysan5)
-*
-********************************************************************************************/
 
 #include "raylib.h"
 #include "screens.h"    // NOTE: Declares global (extern) variables and screens functions
