b65ad1fe5d1076a3a5cc37eff9192a3f9a0d847e
Xavier Hampton <xaviermichaelhampton@gmail.com>
2025-08-24 00:52:55 -0500

    added template map

--- src/maps.c
+++ src/maps.c
@@ -21,7 +21,7 @@ void Gridshot(void)
         {
             while (maxFreq > 0){
                 //Attempt to create Target (maxFreq = 50)
-                int x = (rand() % (targetEngine.xVar / 10)) - (int)(targetEngine.xVar / 20);
+                int x = (rand() % (targetEngine.xVar / 10)) - (int)(targetEngine.xVar / (2 * targetEngine.gap));
                 int y = (rand() % (targetEngine.yVar / 10)) + 1;
                 int z = targetEngine.maxZ / 10;
 
@@ -30,9 +30,6 @@ void Gridshot(void)
                 if (!checkInterference(pos)) {
                     Target newTarget = {pos, targetEngine.targetHealth};
                     targets[i] = newTarget;
-
-                    printf("%d %d %d \n",x, y, z );
-
                     break;
                 }
                 maxFreq -= 1;
